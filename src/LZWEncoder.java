360093409f71efdaa873726ff74be904cb4426af
Tyler-Weigand <tweigand1@hwemail.com>
2020-09-02 13:26:37 -0700

    Update LZWEncoder.java

--- src/LZWEncoder.java
+++ src/LZWEncoder.java
@@ -2,10 +2,13 @@ import java.util.*;
 import java.io.*;
 
 public class LZWEncoder {
+	//codemap is a hashmap that stores the LZW table
 	private HashMap<String, Integer> codemap = new HashMap<String, Integer>();
+	//lastindex stores the last number that we added to the table
 	private int lastindex;
 	
 	public LZWEncoder() {
+		//add a-z as 0-25 in the table
 		for (int i = 0; i<26; i++) {
 			codemap.put(Character.toString((char)(i+97)), i); //what if not all lowercase letters?
 		}
@@ -14,16 +17,23 @@ public class LZWEncoder {
 	
 	
 	public void encode(String inputFile, String outputFile) throws IOException{
+		//the final output to the file
+		String output = "";
+		//a buffer storing the chars that we're trying to determine the equivalent number to
+		String buffer = "";
 		BufferedReader reader = new BufferedReader(new FileReader(inputFile));
 		int inputCharNum = reader.read();
-		String output = "";
-		String buffer = Character.toString((char)inputCharNum);
 		
 		while(inputCharNum != -1) {
+			buffer += Character.toString((char)inputCharNum);
 			
 			
 			inputCharNum = reader.read();
-			buffer += Character.toString((char)inputCharNum);
 		}
+		
+		BufferedWriter outputWriter = new BufferedWriter(new FileWriter(new File(outputFile)));
+		outputWriter.write(output);
+		reader.close();
+		outputWriter.close();
 	}
 }
