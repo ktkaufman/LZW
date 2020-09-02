bdc74dbc03ffb45aa642cdf0897a75d6db1f8a45
Tyler-Weigand <tweigand1@hwemail.com>
2020-09-02 13:01:52 -0700

    Update LZWEncoder.java

--- src/LZWEncoder.java
+++ src/LZWEncoder.java
@@ -2,10 +2,18 @@ import java.util.*;
 import java.io.*;
 
 public class LZWEncoder {
+	
 	private HashMap<Integer, String> codemap = new HashMap<Integer, String>();
 	private int lastindex = 25;
 	
-	public void encode(String inputFile, String outputFile) {
-		
+	
+	public void encode(String inputFile, String outputFile) throws IOException{
+		BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+		int inputCharNum = reader.read();
+		while(inputCharNum != -1) {
+			
+			
+			inputcharNum = reader.read();
+		}
 	}
 }
