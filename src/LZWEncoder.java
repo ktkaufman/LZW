5a3a9448982d3c4d70780673631c3ebb3b27cb25
kcheng902 <47684688+kcheng902@users.noreply.github.com>
2020-09-02 13:02:38 -0700

    constructor

--- src/LZWEncoder.java
+++ src/LZWEncoder.java
@@ -2,8 +2,15 @@ import java.util.*;
 import java.io.*;
 
 public class LZWEncoder {
-	private HashMap<Integer, String> codemap = new HashMap<Integer, String>();
-	private int lastindex = 25;
+	private HashMap<String, Integer> codemap = new HashMap<String, Integer>();
+	private int lastindex;
+	
+	public LZWEncoder() {
+		for (int i = 0; i<26; i++) {
+			codemap.put(Character.toString((char)(i+97)), i); //what if not all lowercase letters?
+		}
+		lastindex = 25;
+	}
 	
 	public void encode(String inputFile, String outputFile) {
 		
