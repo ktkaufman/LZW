7a61dc71163d8915443e68871c60ba27802aba84
ktkaufman <70167517+ktkaufman@users.noreply.github.com>
2020-09-15 10:40:12 -0700

    Update LZWDecoder.java

--- LZWDecoder.java
+++ LZWDecoder.java
@@ -1,5 +1,11 @@
 import java.io.*;
 import java.util.*;
-public class LZWDecoder {
-
+public class LZWDecoder 
+{
+	public LZWDecoder()
+	{
+		
+	}
+	public void decode(String imputFile, String outputFile)
 }
+
