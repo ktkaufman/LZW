af56597d40919f3e2d3744261f0886f005cccc38
ktkaufman <70167517+ktkaufman@users.noreply.github.com>
2020-09-15 10:12:15 -0700

    Create LZWTester2.java

---
+++ LZWTester2.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class LZWTester2 
+{
+	public static void main (String [] args) throws IOException
+	{
+		LZWEncoder test = new LZWEncoder();
+		test.encode("inputTest.txt","outputTest.txt");
+	}
+	
+
+}
