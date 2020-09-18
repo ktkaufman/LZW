d65a0cf687d31f750125fecd00be64a49834bdde
Isaiah Jeter <ijeter1@hwemail.com>
2020-09-18 09:21:48 -0700

    Update LZWTester2.java

--- LZWTester2.java
+++ LZWTester2.java
@@ -4,7 +4,9 @@ public class LZWTester2
 	public static void main (String [] args) throws IOException
 	{
 		LZWEncoder test = new LZWEncoder();
+		LZWDecoder decoder=newLZWDecoder();
 		test.encode("inputTest.txt","outputTest.txt");
+		decoder.decode("outputTest.txt","newOutput.txt")
 	}
 	
 
