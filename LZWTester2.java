f709616f6dc17a30d73b403b96b06cb74ed466c9
ktkaufman <70167517+ktkaufman@users.noreply.github.com>
2020-09-18 09:24:42 -0700

    More bugfixes

--- LZWTester2.java
+++ LZWTester2.java
@@ -4,9 +4,9 @@ public class LZWTester2
 	public static void main (String [] args) throws IOException
 	{
 		LZWEncoder test = new LZWEncoder();
-		LZWDecoder decoder=newLZWDecoder();
+		LZWDecoder decoder=new LZWDecoder();
 		test.encode("inputTest.txt","outputTest.txt");
-		decoder.decode("outputTest.txt","newOutput.txt")
+		decoder.decode("outputTest.txt","newOutput.txt");
 	}
 	
 
